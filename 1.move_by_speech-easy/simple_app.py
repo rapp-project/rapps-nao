979dc7986c66bf953ecfc7da8aae26ac8d9cf296
Manos Tsardoulias <etsardou@gmail.com>
2016-06-01 14:00:43 +0300

    Application 1 updated

--- 1.move_by_speech-easy/simple_app.py
+++ 1.move_by_speech-easy/simple_app.py
@@ -8,15 +8,20 @@ rh = RappRobot()
 # Adjust the NAO master volume and ask for instructions. The valid commands are 'stand' and 'sit' and NAO waits for 5 seconds
 rh.audio.setVolume(50)
 rh.audio.speak("Hello there! What do you want me to do? I can sit or get up.")
-[r, e] = rh.audio.speechDetection(['sit', 'get up'], 5)
+res = rh.audio.speechDetection(['sit', 'get up'], 5)
+print res
+word = ''
+inner_word = ''
+if res['error'] == None:
+    word = res['word']
 
 # Check which command was dictated by the human
-if r[0] == 'sit':
+if word == 'sit':
     # The motors must be enabled for NAO to move
     rh.motion.enableMotors()
     # NAO sits with 75% of its maximum speed
     rh.humanoid_motion.goToPosture('Sit', 0.75)
-elif r[0] == 'get up':
+elif word == 'get up':
     # The motors must be enabled for NAO to move
     rh.motion.enableMotors()
     # NAO stands with 75% of its maximum speed
@@ -27,15 +32,21 @@ else:
 
 # Ask the human what movement to do: move the hands or the head?
 rh.audio.speak("Do you want me to move my arms or my head?")
-[r, e] = rh.audio.speechDetection(['arms', 'head'], 5)
+res = rh.audio.speechDetection(['arms', 'head'], 5)
+print res
+if res['error'] == None:
+    word = res['word']
 
 rh.motion.enableMotors()
-if r[0] == 'arms':
+if word == 'arms':
     rh.audio.speak("Do you want me to open the left or right hand?")
-    [res, err] = rh.audio.speechDetection(['left', 'right'], 5)
-    if res[0] == 'left':
+    res = rh.audio.speechDetection(['left', 'right'], 5)
+    print res
+    if res['error'] == None:
+        inner_word = res['word']
+    if inner_word == 'left':
         rh.humanoid_motion.openHand('Left')
-    elif res[0] == 'right':
+    elif inner_word == 'right':
         rh.humanoid_motion.openHand('Right')
     else:
         pass
@@ -43,15 +54,17 @@ if r[0] == 'arms':
     rh.audio.speak("I will close my hands now")
     rh.humanoid_motion.closeHand('Right')
     rh.humanoid_motion.closeHand('Left')
-elif r[0] == 'head':
+elif word == 'head':
     rh.audio.speak("Do you want me to turn my head left or right?")
-    [res, err] = rh.audio.speechDetection(['left', 'right'], 5)
-
+    res = rh.audio.speechDetection(['left', 'right'], 5)
+    print res
+    if res['error'] == None:
+        inner_word = res['word']
     # The head moves by 0.4 rads left or right with 50% of its maximum speed
-    if res[0] == 'left':
-        rh.humanoid_motion.setJointAngles(['HeadYaw'], [-0.4], 0.5)
-    elif res[0] == 'right':
+    if inner_word == 'left':
         rh.humanoid_motion.setJointAngles(['HeadYaw'], [0.4], 0.5)
+    elif inner_word == 'right':
+        rh.humanoid_motion.setJointAngles(['HeadYaw'], [-0.4], 0.5)
     else:
         pass
 
